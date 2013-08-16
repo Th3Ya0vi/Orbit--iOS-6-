b41922de27a0ac15420e75ea14ca3a51f9d8972d
hamdanjaveed <hamdan@hamdanjaveed.com>
2013-08-15 21:54:57 -0400

    Cleared out the gameplay scene in preparation for features
    Removed debugging code

--- Orbit/GameplayScene.h
+++ Orbit/GameplayScene.h
@@ -6,7 +6,6 @@
 //  Copyright (c) 2013 Hamdan Javeed. All rights reserved.
 //
 //  The scene that contains the gameplay layers. This scene is the game.
-//  It draws the icon for debugging purposes.
 
 #import "CCScene.h"
 
--- Orbit/GameplayScene.m
+++ Orbit/GameplayScene.m
@@ -6,19 +6,16 @@
 //  Copyright (c) 2013 Hamdan Javeed. All rights reserved.
 //
 //  The scene that contains the gameplay layers. This scene is the game.
-//  It draws the icon for debugging purposes.
 
 #import "GameplayScene.h"
 
 @implementation GameplayScene
 
-// create an icon sprite and add it as a child
+// initialize gameplay scene
 - (id)init {
     self = [super init];
     if (self) {
-        CCSprite *tempIconSprite = [CCSprite spriteWithFile:@"Icon.png"];
-        tempIconSprite.position = CENTER_POINT;
-        [self addChild:tempIconSprite];
+        
     }
     return self;
 }
